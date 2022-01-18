873e9a8efb7f4753d5e1555c5ab33cfbf5fd09b0
chupdachups <chupdachups@gmail.com>
2022-01-18 16:49:13 +0900

    source push

---
+++ src/main/java/com/example/webserver/SpringCloudWebserverApplication.java
@@ -0,0 +1,15 @@
+package com.example.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudWebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudWebserverApplication.class, args);
+	}
+
+}
