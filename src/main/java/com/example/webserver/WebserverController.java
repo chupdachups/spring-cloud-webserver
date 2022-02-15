3d3af258ca0859b575c498b7799acd8c231772d8
chupdachups <chupdachups@gmail.com>
2022-02-15 20:19:56 +0900

    circuitbreaker test method 추가 및 remove

--- src/main/java/com/example/webserver/WebserverController.java
+++ src/main/java/com/example/webserver/WebserverController.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,5 +35,29 @@ public class WebserverController {
 		String msg = "("+headers.get("host")+":"+port+")"+param + " => Working successfully !!!";
 		log.info("### Sent: " + msg);
 		return msg;
-	} 
+	}
+    
+    @GetMapping("/testSCCB/{param}")
+	public String testSCCB(
+		@PathVariable String param, 
+		@RequestHeader HttpHeaders headers,
+		@CookieValue(name = "httpclient-type", required=false, defaultValue="undefined") String httpClientType) throws Exception {
+		
+		log.info(">>>> Cookie 'httpclient-type={}'", httpClientType);
+
+		headers.forEach((key, value) -> {
+			log.info(String.format(">>>>> Header '%s' => %s", key, value));
+		});
+		
+        log.info("### Received: /testSCCB/" + param);
+		
+		String msg = "("+headers.get("host")+":"+port+")"+param + " => Working successfully !!!";
+		log.info("### Sent: " + msg);
+		
+		if(param.equals("test")) {
+			throw  new RuntimeException("Force Error");
+		}
+		return msg;
+	}
+
 }
